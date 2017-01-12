2ac0f0d9e7859efa917447ff71d2592f0c0b03c8
Luke Ghenco <lukeghenco@gmail.com>
2017-01-12 15:58:09 -0500

    You can run the server now with 'npm run server' and fixed app.js bodyParser error.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
-const bodyParse = require('body-parser');
+const bodyParser = require('body-parser');
+
+// Define express app
+const app = express()
 
 // Import Routes
 const index = require('./routes/index');
