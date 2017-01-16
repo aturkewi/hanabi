f5ce40e1f71eb94039ee33c39ba96cd5174d2171
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-16 13:10:16 -0500

    Fix hanabiActions per Luke request

--- client/src/actions/hanabiActions.js
+++ client/src/actions/hanabiActions.js
@@ -1,3 +1,8 @@
+const getRandCard = (deck) =>{
+  const randNum = Math.floor(Math.random()*deck.length);
+  return deck.splice(randNum,1)[0]
+}
+
 export function resetGame(){
   return {type: "RESET_GAME"}
 }
@@ -8,7 +13,7 @@ export function addPlayer(playerName){
 
 export function startGame(originalPlayers, originalDeck){
   const deck = [...originalDeck];
-  let cardCount = undefined;
+  let cardCount;
   if (originalPlayers.length > 1 && originalPlayers.length < 4){
     cardCount = 5;
   }else if (originalPlayers.length >= 4 && originalPlayers.length <= 5) {
@@ -16,15 +21,10 @@ export function startGame(originalPlayers, originalDeck){
   }
   const players = originalPlayers.map(p => {
     const hand = [];
-    for(let i=0; i < cardCount; i++ ){
+    for(let i = 0; i < cardCount; i++ ){
       hand.push(getRandCard(deck));
     }
     return Object.assign({}, p, { hand });
   });
   return {type: "START_GAME", players, deck}
-}
-
-const getRandCard = (deck) =>{
-  let randNum = Math.floor(Math.random()*deck.length);
-  return deck.splice(randNum,1)[0]
 }
