b99afba7155b39269936953c0500eb0730ce2b90
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-16 14:10:58 -0500

    Add simple grid

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,21 @@
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import './simple-grid.css'
 import { addPlayer, startGame } from './actions/hanabiActions'
 import Game from './components/Game'
 
 const App = props => {
   return (
     <div className="App">
-      <Game
-        startGame={props.actions.startGame}
-        addPlayer={props.actions.addPlayer}
-        game = {props.game}
-        />
+      <div className="container">
+        <Game
+          startGame={props.actions.startGame}
+          addPlayer={props.actions.addPlayer}
+          game={props.game}
+          />
+      </div>
     </div>
   );
 }
