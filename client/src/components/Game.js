9614c8519feb02c9ef2e1f7af9bdc37be553c728
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-01-23 17:46:54 -0500

    Display played and discarded cards

--- client/src/components/Game.js
+++ client/src/components/Game.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Player from './Player';
+import Card from './Card';
 
 import AddPlayer from './AddPlayer';
 
@@ -10,6 +11,10 @@ export default (props) => {
     event.preventDefault();
     props.actions.discardCard(currentPlayer, discardedCard, clueCounter, deck, players)
   }
+  const colors = ['Blue', 'Green', 'Red', 'White', 'Yellow']
+  // const sortByColor = cardSet => {
+  //   
+  // }
   return(
     <div>
       <div className="row">
@@ -17,11 +22,14 @@ export default (props) => {
         {(players.length > 1) ? <button onClick={props.actions.startGame.bind(null, players, deck)}>Start Game</button> : ''}
       </div>
       <div className="row">
-        <div className="col-6">
+        <div className="col-4">
           <h3>Clue Remaining: {props.game.clueCounter}</h3>
         </div>
-        <div className="col-6">
+        <div className="col-4">
           <h3>Misses Remaining: {props.game.missesRemaining}</h3>
+        </div>
+        <div className="col-4">
+          <h3>Cards Remaining: {props.game.deck.length}</h3>
         </div>
       </div>
       <div className="row">      
@@ -34,6 +42,38 @@ export default (props) => {
             />
           </div>)})
         }
+      </div>
+      <div className="row">
+        <div className="col-6">
+          <h3>Played Cards</h3>
+          <div className="row">
+            {colors.map(color => (
+              <div className="col-2">
+                <h4>{color}</h4>
+                <ul className="cards">
+                  {props.game.played.filter(c => c.color === color).map((card,i) => (
+                    <Card card={card} key={i}/>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="col-6">
+          <h3>Discarded Cards</h3>
+          <div className="row">
+            {colors.map(color => (
+              <div className="col-2">
+                <h4>{color}</h4>
+                <ul className="cards">
+                  {props.game.discard.filter(c => c.color === color).map((card,i) => (
+                    <Card card={card} key={i}/>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   )
