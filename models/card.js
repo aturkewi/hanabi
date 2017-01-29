31c6ccd8f27b508d4f2ace987b2cc3eaf209caf6
Luke Ghenco <lukeghenco@gmail.com>
2017-01-29 15:22:55 -0500

    add Card seeder file

--- models/card.js
+++ models/card.js
@@ -1,13 +1,9 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var Card = sequelize.define('Card', {
+module.exports = (sequelize, DataTypes) => {
+
+  const Card = sequelize.define('Card', {
     color: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
+  }, {} );
+
   return Card;
 };
