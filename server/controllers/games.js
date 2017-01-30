54733c9aa16efd1a39e1da60c22ddaf56b7245ee
Luke Ghenco <lukeghenco@gmail.com>
2017-01-30 09:49:02 -0500

    when a game is created it is returned with a currentPlayerId with a hand and that hands user

--- server/controllers/games.js
+++ server/controllers/games.js
@@ -1,10 +1,33 @@
-const { Game } = require('../models');
+const { Game, Hand, User } = require('../models');
+const pry = require('pryjs');
 
 const create = (req, res, next) => {
-  console.log(res.token);
   Game.create({
-    currentPlayerId: res.token.id
+    currentPlayerId: res.token.id,
+    Hands: [
+      {
+        UserId: res.token.id
+      }
+    ]
+  }, {
+    include: [ Hand ]
   })
+  .then(game => Game.findOne({
+    where: {
+      id: game.id
+    },
+    include: [
+      {
+        model: Hand,
+        include: [
+          {
+            model: User,
+            attributes: ["username", "email", "firstName", "lastName"]
+          }
+        ]
+      }
+    ]
+  }))
   .then(game => res.status(200).json(game))
   .catch(err => res.json(err));
 }
