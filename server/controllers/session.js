37050d075a16da203e37c47ad282089dde24c051
Luke Ghenco <lukeghenco@gmail.com>
2017-02-05 05:39:17 -0500

    change comparePassword to authenticate

--- server/controllers/session.js
+++ server/controllers/session.js
@@ -6,7 +6,7 @@ const login = (req, res, next) => {
   User.findOne({ email: req.body.user.email })
   .then((user) => {
     res.user = user;
-    user.comparePassword(req.body.user.password);
+    user.authenticate(req.body.user.password);
   })
   .then(response => res.status(200).json({ token: Auth.createToken(res.user) }))
   .catch(err => res.status(500).json(err));
