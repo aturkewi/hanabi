bb80fcf231832b103efef175035e5e3a61c366ff
Luke Ghenco <lukeghenco@gmail.com>
2017-02-05 10:23:08 -0500

    remove sequelize cli

---
+++ routes/api/v1/games.js
@@ -0,0 +1,39 @@
+import _ from 'lodash';
+
+module.exports = (app) => {
+  const { User, Game, Hand } = app.db.models;
+  const authenticate = app.services.auth.passport.authenticate;
+  const currentUser = app.services.auth.jwtAuth.currentUser;
+
+  app.route("/api/v1/games")
+    .all(authenticate(), currentUser)
+    .post((req, res) => {
+      Game
+        .create({
+          currentPlayerId: res.token.id,
+          hands: [
+            { userId: res.token.id }
+          ]
+        }, {
+          include: [ Hand ]
+        })
+        .then(game => Game.findOne({
+          where: {
+            id: game.id
+          },
+          include: [
+            {
+              model: Hand,
+              include: [
+                {
+                  model: User,
+                  attributes: ["username", "email", "firstName", "lastName"]
+                }
+              ]
+            }
+          ]
+        }))
+        .then(game => res.status(200).json(game))
+        .catch(err => res.json(err));
+    })
+};
