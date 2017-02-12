508eb5768f4867ba37e9539ef8acca4d06c28e37
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-02-12 17:14:28 -0500

    Changes state of games reducer from obj to array

--- client/src/reducers/gamesReducer.js
+++ client/src/reducers/gamesReducer.js
@@ -1,11 +1,9 @@
-export default (state = {
-  games: [],
-}, action) => {
+export default (state = [], action) => {
   switch(action.type){
     case "CREATE_GAME_SUCCESS":
-      return Object.assign({}, state, { games: state.games.concat(action.game) });
+      return [...state, action.game];
     case "ADD_GAMES":
-      return Object.assign({}, state, {games: action.games})
+      return action.games
     default:
       return state;
   }
