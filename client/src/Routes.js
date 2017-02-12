655dc6246a70a5983567de60a6784d9d2aa5fe9b
Avidor Turkewitz <aviturkewitz@gmail.com>
2017-02-12 17:37:39 -0500

    Add route for a single game

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -5,13 +5,16 @@ import Home from './components/Home';
 import SignUp from './components/auth/SignUp';
 import Login from './components/auth/Login';
 import Games from './components/games/Games';
+import Game from './components/games/Game'
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Home} />
     <Route path="/signup" component={SignUp}/>
     <Route path="/login" component={Login}/>
-    <Route path="/games" component={Games}/>
+    <Route path="/games" component={Games}>
+        <Route path="/games/:gameId" component={Game}></Route>
+    </Route>
   </Route>
 )
 
