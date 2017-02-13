f4f7897c2b8bbe7c7d2fd2b25a5d168854d84b54
Luke Ghenco <lukeghenco@gmail.com>
2017-02-13 16:54:03 -0500

    add show function to gameService

--- client/src/services/gameService.js
+++ client/src/services/gameService.js
@@ -16,6 +16,22 @@ const index = () => {
     .catch(err => err);
 }
 
+const show = (gameId) => {
+  return fetch(`/api/v1/games/${gameId}`, {
+    method: 'GET',
+    headers: {
+      'Authorization': `JWT ${getToken()}`,
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then((response) => {
+      return response.json()
+    })
+    .then(data => data)
+    .catch(err => err);
+}
+
 const create = (title) => {
   /*
     NOTE: THIS will only work for dev. This needs to be a dynamic URL for production
@@ -40,4 +56,5 @@ const create = (title) => {
 module.exports = {
   create,
   index,
+  show,
 }
