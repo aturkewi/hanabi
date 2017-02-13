8f484792dcf0caaf4aa77cd53b2b9d9188abefe8
Luke Ghenco <lukeghenco@gmail.com>
2017-02-13 16:52:55 -0500

    remove unneeded code from main Index.js component

--- client/src/index.js
+++ client/src/index.js
@@ -4,13 +4,8 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router'
 import Routes from './Routes'
 import configureStore from './store/configureStore';
-// import Game from './services/gameService';
-import { getGames } from './actions/gamesActions';
-// import { resetGame } from './actions/hanabiActions';
 
 const store = configureStore();
-
-store.dispatch(getGames());
 
 ReactDOM.render(
   <Provider store={store}>
